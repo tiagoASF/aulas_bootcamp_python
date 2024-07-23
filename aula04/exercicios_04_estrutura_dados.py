aa02c4a31c1e15fba248981456cbc9070095628c
Tiago Fialho <tiago.chem@gmail.com>
2024-07-23 15:56:23 -0300

    exercicio 4_01 completo

--- aula04/exercicios_04_estrutura_dados.py
+++ aula04/exercicios_04_estrutura_dados.py
@@ -1,4 +1,10 @@
+import math
+
 # Exercicio 01 - Crie uma lista com os números de 1 a 10 e use um loop para imprimir cada número elevado ao quadrado.
+numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+for numero in numeros:
+    print(math.pow(numero, 2))
 
 # Exercicio 02 - Dada a lista ["Python", "Java", "C++", "JavaScript"], remova o item "C++" e adicione "Ruby".
 
