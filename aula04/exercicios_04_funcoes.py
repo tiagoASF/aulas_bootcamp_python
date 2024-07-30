73fae9db6c92b1b898931d137c9b0a79a18b6c73
Tiago Fialho <tiago.chem@gmail.com>
2024-07-30 10:31:07 -0300

    exercicios de funcoes criados

---
+++ aula04/exercicios_04_funcoes.py
@@ -0,0 +1,27 @@
+# def normalizar_nome(nome: str) -> str:
+#     return nome.strip().lower()
+
+# nomes = [" Alice", "BOB", " Carlos  " ]
+# nomes_normalizados = []
+
+# for nome in nomes:
+#     nomes_normalizados.append(normalizar_nome(nome))
+
+# print(nomes_normalizados)
+
+### OPCAO
+# nomes_normalizados = [normalizar_nome(nome) for nome em nomes]
+
+
+# 01 - Escreva uma função que receba uma lista de números e retorne a soma de todos os números.
+
+# 02 - Crie uma função que receba um número como argumento e retorne True se o número for primo e False caso contrário.
+    
+# 03 - Desenvolva uma função que receba uma string como argumento e retorne essa string revertida.
+
+# 04 - Implemente uma função que receba dois argumentos: uma lista de números e um número. A função deve retornar todas as combinações de pares na lista que somem ao número dado.
+
+# 05 - Escreva uma função que receba um dicionário e retorne uma lista de chaves ordenadas
+
+
+# DESAFIO - Exercicio KPi com dicionarios, type hint e funcoes
