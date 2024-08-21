6348cd5d4d5a6b0de73f4610ec1a27f7477ea3cb
Tiago Fialho <tiago.chem@gmail.com>
2024-08-21 16:21:50 -0300

    exercicio INVERSAO DE STRINGS completo

--- aula04/exercicios_04_funcoes.py
+++ aula04/exercicios_04_funcoes.py
@@ -24,11 +24,11 @@ def normalizar_nome(nome: str) -> str:
 
 # 01 - Escreva uma função que receba uma lista de números e retorne a soma de todos os números.
 
-# def soma_numeros(numeros = []) -> int:
-#     soma = 0
-#     for numero in numeros:
-#         soma += numero
-#     return soma
+def soma_numeros(numeros = []) -> int:
+    soma = 0
+    for numero in numeros:
+        soma += numero
+    return soma
 
 # numeros = [10, 20, 300]
 # soma_total = soma_numeros(numeros)
@@ -61,16 +61,31 @@ def is_primo(numero: int) -> bool:
     return primo
         
     
-numero = int(input("Informe o número a ser testado: "))
-result = is_primo(numero)
+# numero = int(input("Informe o número a ser testado: "))
+# result = is_primo(numero)
 
-if (result):
-    print(f"{numero} é um número primo.")
-else:
-    print(f"{numero} não é primo.")
+# if (result):
+#     print(f"{numero} é um número primo.")
+# else:
+#     print(f"{numero} não é primo.")
 
-print(is_primo.__doc__)
+# print(is_primo.__doc__)
+
 # 03 - Desenvolva uma função que receba uma string como argumento e retorne essa string revertida.
+
+def inverte_string(palavra: str) -> str:
+    
+    """Inverte uma string
+    input: string a ser invertida
+    output: string invertida
+    """
+
+    inverted_string = palavra[::-1]
+    return inverted_string
+
+# result = inverte_string("abacate")
+# print(result)
+
 
 # 04 - Implemente uma função que receba dois argumentos: uma lista de números e um número.
 # A função deve retornar todas as combinações de pares na lista que somem ao número dado.
